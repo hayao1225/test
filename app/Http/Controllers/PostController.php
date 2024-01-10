eb02c35ba21d306a5d2eb619e94a469b1fac7818
hayao1225 <hayao1225@gmail.com>
2024-01-11 00:35:03 +0900

    カリキュラム3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,10 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
     //getPaginateByLimit()はPost.phpで定義したメソッドです
     }
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス
+    }
+
 }
