562214be306a4fdffd822a193df49e271604fe8e
hayao1225 <hayao1225@gmail.com>
2024-01-16 18:58:12 +0900

    カリキュラム６

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -21,6 +21,6 @@
             </div>
             <input type="submit" value="保存"/>
         </form>
-        <div class="back">[<a href="/">back</a>]</div>
+        <div class="back">[<a href="/">戻る</a>]</div>
     </body>
 </html>
